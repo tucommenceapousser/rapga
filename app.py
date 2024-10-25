4b80ee0f4f9d792e1202de8bc0ffaaef6aa78e2b
trhacknon <129875733+tucommenceapousser@users.noreply.github.com>
2024-10-25 09:27:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -9,6 +9,13 @@ MESSAGES_FILE = 'messages.json'
 RAP_TEXTS = []
 MESSAGES = {'messages': []}
 
+def local_css(file_name):
+    with open(file_name) as f:
+        st.markdown(f"<style>{f.read()}</style>", unsafe_allow_html=True)
+
+# Appliquer le CSS personnalisé
+local_css("style.css")
+
 # Make sure the upload folder exists
 if not os.path.exists(UPLOAD_FOLDER):
     os.makedirs(UPLOAD_FOLDER)
